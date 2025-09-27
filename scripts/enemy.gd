4f931ac839c2fed14fd76f436ae3d3c645b70c75
Quentin R <flememl@users.noreply.github.com>
2025-09-27 20:16:32 +0100

    bouncing off walls

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,18 +1,24 @@
 extends CharacterBody2D
 
-const SPEED = 100
-const RANGE = 100
+@export var speed: int
+
 const ORIGIN = Vector2(-250, -128)
+const BOUNDARIES = [-250, 250]
 
 func _ready() -> void:
 	position = ORIGIN
 
 func _physics_process(delta: float) -> void:
 	get_direction()
-	move_and_collide(velocity * delta)
+	move_and_collide(velocity * speed * delta)
 
+var direction = 1
 func get_direction() -> void:
-	velocity.x = move_toward(velocity.x, RANGE, SPEED)
+	if position.x < BOUNDARIES.min():
+		direction = 1
+	elif position.x > BOUNDARIES.max():
+		direction = -1
+	velocity.x = move_toward(velocity.x, direction, speed)
 
 # Note: temporary, needed for scene deletion?
 func _on_death():
