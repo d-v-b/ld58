73ba4fcf5386738b456e241c170a4853ed897c11
Max Pollard <maxpollard@live.co.uk>
2025-09-27 18:31:45 +0100

    changed player movement logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 
+func _ready() -> void:
+	var window_size = get_viewport().get_visible_rect().size * 0.5
+	position.y = (window_size.y * 0.5) - 10
+	print(window_size.y, " ", position.y)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var window_size = get_viewport().get_visible_rect().size * 0.5
+	var movement;
+	
+	var direction := Input.get_axis("move_left", "move_right")
+	
 	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		movement = direction * SPEED * delta
+		var new_position = position.x + movement
+		position.x = clamp(new_position, -window_size.x * 0.5, window_size.x * 0.5)
