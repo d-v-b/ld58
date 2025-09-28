0cf1df9bd26f610d6bbd2c5d14c54a78469f1e6a
Quentin R <flememl@users.noreply.github.com>
2025-09-28 14:54:48 +0100

    Enemy lines will go down when reaching edge of the screen and accelerate when having completed a cycle (travel & return)

--- scripts/enemy_line.gd
+++ scripts/enemy_line.gd
@@ -4,23 +4,31 @@ extends CharacterBody2D
 @export var enemy_padding: int = 1
 @export var enemy_number: int = 10
 @export var enemy_speed: int = 100
+@export var enemy_speed_increment: int = 30
 @export var side_padding: int = 20
 
 var boundaries = Array()
+
+signal end_of_line
+signal end_of_cycle
 
 func _ready() -> void:
 	position = Vector2(0, -250)
 	var line_size: Vector2
 	for n in enemy_number:
+		var enemy = init_enemy(n)
+		line_size.x += enemy.width + enemy_padding
+		line_size.y = enemy.height
+		add_child(enemy)
+	set_shape(line_size)
+	set_boundaries()
+	
+func init_enemy(n: int) -> Enemy:
 		var enemy = enemy_scene.instantiate()
 		var origin = Vector2(-(((enemy.width + enemy_padding) * enemy_number) / 2) + (enemy.width / 2), 0)
 		origin.x += (enemy.width + enemy_padding) * n
 		enemy.set_origin(origin)
-		add_child(enemy)
-		line_size.x += enemy.width + enemy_padding
-		line_size.y = enemy.height
-	set_shape(line_size)
-	set_boundaries()
+		return enemy
 
 func set_shape(line_size: Vector2) -> void:
 	$LineShapeCollisionBox.shape.size = line_size
@@ -32,12 +40,20 @@ func set_boundaries() -> void:
 
 func _physics_process(delta: float) -> void:
 	get_direction()
+	velocity.x = move_toward(velocity.x, direction, enemy_speed)
 	move_and_collide(velocity * enemy_speed * delta)
 
-var direction = 1
 func get_direction() -> void:
 	if position.x < boundaries.min():
-		direction = 1
+		end_of_line.emit()
+		end_of_cycle.emit()
 	elif position.x > boundaries.max():
-		direction = -1
-	velocity.x = move_toward(velocity.x, direction, enemy_speed)
+		end_of_line.emit()
+
+var direction = 1
+func _on_end_of_line() -> void:
+	direction *= -1
+	position.y += $LineShapeCollisionBox.shape.size.y / 2
+
+func _on_end_of_cycle() -> void:
+	enemy_speed += enemy_speed_increment
