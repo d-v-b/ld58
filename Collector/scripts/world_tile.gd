3cd9afe4fbac64a2a90e3b274d5be7cd45f087fd
Max Pollard <maxpollard@live.co.uk>
2025-10-04 18:19:58 +0100

    stuff

--- Collector/scripts/world_tile.gd
+++ Collector/scripts/world_tile.gd
@@ -4,6 +4,8 @@ class_name world_tile
 const _size = Vector2i(64, 64)
 const _offset = Vector2i(_size.x * -0.5, 0)
 
+const mining_block = preload("res://scenes/mining_block.tscn")
+
 var grid := []
 
 func _ready() -> void:
@@ -13,6 +15,10 @@ func _ready() -> void:
 			var _cell = cell.new()
 			var rand_value = randi_range(0, 4)
 			_cell.value = 0 if (rand_value == 0) else 1
+			
+			#var mining_blcok = mining_block.instantiate()
+			#mining_blcok.position = Vector2(64 * x, 64 * y)
+			#add_child(mining_blcok)
 			
 			row.append(_cell)
 		grid.append(row)
@@ -25,4 +31,6 @@ func _ready() -> void:
 			else:
 				set_cell(Vector2i(x, y) + _offset, 1, Vector2i(-1, -1))
 			
-#func get_cell_coord_at_world_pos(vec2 world_position):
+func get_cell_coord_at_world_pos(vec2 world_position):
+	
+func get_cell(coord)
