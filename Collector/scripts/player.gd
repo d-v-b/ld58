d319c4b302280e001d15d3e0a153e0794afacc89
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-10-04 23:00:13 +0200

    add death logic

--- Collector/scripts/player.gd
+++ Collector/scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+signal died
 @export var speed = 300.0
 @export var jump_velocity = -400.0
 
+var is_dead = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
@@ -16,17 +18,25 @@ func _physics_process(delta):
 		if $MiningBlockSelector.current:
 			$MiningBlockSelector.current.mine.emit()
 	
+	if Input.is_action_just_pressed("action_die"):
+		die()
+	
 	handle_input()
 	move_and_slide()
 
 func handle_input():
-	var input_dir = 0
-	if Input.is_action_pressed("ui_left"):
-		input_dir -= 1
-	if Input.is_action_pressed("ui_right"):
-		input_dir += 1
-	
-	if input_dir != 0:
-		velocity.x = input_dir * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+	if not is_dead:
+		var input_dir = 0
+		if Input.is_action_pressed("ui_left"):
+			input_dir -= 1
+		if Input.is_action_pressed("ui_right"):
+			input_dir += 1
+		if input_dir != 0:
+			velocity.x = input_dir * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+
+func die():
+	is_dead = true
+	died.emit()
+	%AnimationPlayer.play("player_death")
