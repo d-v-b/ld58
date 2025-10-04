774cad038ad6f078449715ba6b4bec11669c4cb6
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-10-04 23:13:52 +0200

    Revert "Merge branch 'main' of https://github.com/d-v-b/ld58 into feat/death"
    This reverts commit be80f326218a6ab9c3e0c9a022d1fe36cd6eae70, reversing
    changes made to d319c4b302280e001d15d3e0a153e0794afacc89.

--- Collector/scripts/player.gd
+++ Collector/scripts/player.gd
@@ -1,36 +1,40 @@
 extends CharacterBody2D
-class_name gamejam_player
 
 signal died
 @export var speed = 300.0
 @export var jump_velocity = -400.0
-var is_dead: bool = false
-var direction = 1
 
+var is_dead = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-signal change_direction
-
 func _physics_process(delta):
-	if not is_dead:
-		if not is_on_floor():
-			velocity.y += gravity * delta
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("action_jump") and is_on_floor():
+		velocity.y = jump_velocity
 		
-		if direction != Input.get_axis("move_left", "move_right"):
-			direction = Input.get_axis("move_left", "move_right")
+	if Input.is_action_just_pressed("action_mine"):
+		if $MiningBlockSelector.current:
+			$MiningBlockSelector.current.mine.emit()
+	
+	if Input.is_action_just_pressed("action_die"):
+		die()
+	
+	handle_input()
+	move_and_slide()
 
-		velocity.x = direction * speed
-		
-		if Input.is_action_just_pressed("action_jump") and is_on_floor():
-			velocity.y = jump_velocity
-			
-		if Input.is_action_just_pressed("action_mine"):
-			if $MiningBlockSelector.current:
-				$MiningBlockSelector.current.mine.emit()
-		
-		if Input.is_action_just_pressed("action_die"):
-			die()
-		move_and_slide()
+func handle_input():
+	if not is_dead:
+		var input_dir = 0
+		if Input.is_action_pressed("ui_left"):
+			input_dir -= 1
+		if Input.is_action_pressed("ui_right"):
+			input_dir += 1
+		if input_dir != 0:
+			velocity.x = input_dir * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
 
 func die():
 	is_dead = true
--- Collector/scripts/world_tile.gd
+++ Collector/scripts/world_tile.gd
@@ -10,13 +10,11 @@ var grid := []
 
 func _ready() -> void:
 	for y in _size.y:
-		var material = 1 if (y < 4) else 2
-		
 		var row := []
 		for x in _size.x:
 			var _cell = cell.new()
 			var rand_value = randi_range(0, 4)
-			_cell.value = 0 if (rand_value == 0) else material
+			_cell.value = 0 if (rand_value == 0) else 1
 			
 			#var mining_blcok = mining_block.instantiate()
 			#mining_blcok.position = Vector2(64 * x, 64 * y)
@@ -26,59 +24,15 @@ func _ready() -> void:
 		grid.append(row)
 	
 	
-	for y in range(-1, _size.y):
+	for y in _size.y:
 		for x in _size.x:
-			if y == -1:
-				set_cell(Vector2i(x, y) + _offset, 1, Vector2i(4, 0)  + choose_air_offset(x, y))
-			elif grid[y][x].value == 1:
-				set_cell(Vector2i(x, y) + _offset, 1, Vector2i(1, 2) + choose_tile_offset(x, y))
-			elif grid[y][x].value == 2:
-				set_cell(Vector2i(x, y) + _offset, 1, Vector2i(5, 2)  + choose_tile_offset(x, y))
+			if grid[y][x].value == 1:
+				set_cell(Vector2i(x, y) + _offset, 1, Vector2i(1, 2))
 			else:
-				set_cell(Vector2i(x, y) + _offset, 1, Vector2i(4, 0)  + choose_air_offset(x, y))
-				
+				set_cell(Vector2i(x, y) + _offset, 1, Vector2i(-1, -1))
 			
 func get_cell_coord_at_world_pos(world_position):
 	return
 	
 func get_cell(coord):
 	return
-
-
-func choose_tile_offset(x: int, y: int) -> Vector2i:
-	var max_y := grid.size() - 1
-	if max_y < 0:
-		return Vector2i.ZERO
-	var max_x = grid[0].size() - 1
-
-	var top_free = y == 0 or (y > 0 and grid[y - 1][x].value == 0)
-	var bot_free = y < max_y and grid[y + 1][x].value == 0
-	var lft_free = x > 0 and grid[y][x - 1].value == 0
-	var rgt_free = x < max_x and grid[y][x + 1].value == 0
-
-	if top_free:
-		if lft_free:
-			return Vector2i(-1, -1)
-		elif not rgt_free:
-			return Vector2i(0, -1)
-		return Vector2i(1, -1)
-	elif bot_free:
-		if lft_free:
-			return Vector2i(-1, 1)
-		elif not rgt_free:
-			return Vector2i(0, 1)
-		return Vector2i(1, 1)
-	elif lft_free:
-		return Vector2i(-1, 0)
-	elif rgt_free:
-		return Vector2i(1, 0)
-	return Vector2i(0, 0)
-	
-func choose_air_offset(x: int, y: int) -> Vector2i:
-	var max_y := grid.size() - 1
-	var bot_grass = y < max_y and grid[y + 1][x].value == 1
-
-	if (bot_grass):
-		return Vector2i(-randi() % 5, 0)
-	
-	return Vector2i(0, 0)
