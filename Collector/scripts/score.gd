688a101b65bdcf3e4fb794041b23738b438ce708
n1cxyz <owerwa@gmail.com>
2025-10-06 12:41:13 +0200

    added score

--- Collector/scripts/score.gd
+++ Collector/scripts/score.gd
@@ -1,6 +1,7 @@
 extends RichTextLabel
 
 func _process(delta):
-	text = "SCORE [color=red]%d[/color]" % get_node("/root/Main").score
-	var main = get_node("/root/Main")
+	#text = "SCORE [color=red]%d[/color]" % Globals.score
+	text = str(Globals.score)
+	#var main = get_node("/root/Main")
 	#text = str(main.diamonds_collected, " / ", main.diamonds)
