3619aee04b02722a8a89e4bdfbc3c82506c6a33f
n1cxyz <owerwa@gmail.com>
2025-10-06 12:50:05 +0200

    updated score

--- Collector/scripts/score.gd
+++
@@ -1,7 +0,0 @@
-extends RichTextLabel
-
-func _process(delta):
-	#text = "SCORE [color=red]%d[/color]" % Globals.score
-	text = str(Globals.score)
-	#var main = get_node("/root/Main")
-	#text = str(main.diamonds_collected, " / ", main.diamonds)
